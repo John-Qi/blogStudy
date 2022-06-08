abcf71fdbda44943d58c5d7efa84e7f357b3e596
qijq <qijq@amberdata.cn>
2022-06-08 17:23:18 +0800

    登录接口配置

--- src/router/user.js
+++ src/router/user.js
@@ -2,14 +2,20 @@ const {loginCheck} =require('../controller/user')
 const {SuccessModel,ErrorModel}=require('../model/resModel')
 const handleUserRouter =(req,res) =>{
     const method=req.method
-    if (method === "POST" &&req.path === "/api/user/login") {
-        const {username,password}=req.body
-       const result=loginCheck(username,password)
-       if (result) {
-           return new SuccessModel('登录成功')
-       }else {
-           return new ErrorModel('密码或用户名错误')
-       }
+    if (method === "POST" && req.path === "/api/user/login") {
+        const {
+            username,
+            password
+        } = req.body
+        const result = loginCheck(username, password)
+        return result.then(data => {
+            if (data.username) {
+                    return new SuccessModel('登录成功')
+            }else {
+                return new ErrorModel('密码或用户名错误')
+            }
+        })
+
     }
 }
 module.exports =handleUserRouter
